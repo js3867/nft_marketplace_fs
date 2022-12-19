2da94fa24f498e9f2172fae4f973b2490b4ab073
js3867 <83634235+js3867@users.noreply.github.com>
2022-12-19 22:44:56 +0800

    user profile menu done

--- Front_End/nft-marketplace-frontend/components/NavBar/Profile/Profile.jsx
+++ Front_End/nft-marketplace-frontend/components/NavBar/Profile/Profile.jsx
@@ -1,10 +1,71 @@
 import React from "react"
+import Image from "next/image"
+import Link from "next/link"
+import { FaUserAlt, FaImag, FaUserEdit, FaRegImage } from "react-icons/fa" // font awesome
+import { MdHelpCenter } from "react-icons/md"
+import { TbDownloadOff, TbDownload } from "react-icons/tb"
 
 // INTERNAL IMPORT
 import Style from "./Profile.module.css"
+import images from "../../../img"
 
 const Profile = () => {
-  return <div>Profile</div>
+  return (
+    <div className={Style.profile}>
+      <div className={Style.profile_account}>
+        <Image
+          src={images.user1}
+          alt="user profile"
+          width={50}
+          height={50}
+          className={Style.profile_account_image}
+        />
+
+        <div className={Style.profile_account_info}>
+          <p>Shoaib Bhai</p>
+          <small>X03345345345...</small>
+        </div>
+      </div>
+
+      <div className={Style.profile_menu}>
+        <div className={Style.profile_menu_one}>
+          <div className={Style.profile_menu_one_item}>
+            <FaUserAlt />
+            <p>
+              <Link href={{ pathname: "/myprofile" }}>My Profile</Link>
+            </p>
+          </div>
+          <div className={Style.profile_menu_one_item}>
+            <FaRegImage />
+            <p>
+              <Link href={{ pathname: "/my-items" }}>My Items</Link>
+            </p>
+          </div>
+          <div className={Style.profile_menu_one_item}>
+            <FaUserEdit />
+            <p>
+              <Link href={{ pathname: "/edit-profile" }}>Edit Profile</Link>
+            </p>
+          </div>
+        </div>
+
+        <div className={Style.profile_menu_two}>
+          <div className={Style.profile_menu_one_item}>
+            <MdHelpCenter />
+            <p>
+              <Link href={{ pathname: "/help" }}>Help</Link>
+            </p>
+          </div>
+          <div className={Style.profile_menu_one_item}>
+            <TbDownload />
+            <p>
+              <Link href={{ pathname: "/disconnect" }}>Disconnect</Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Profile
