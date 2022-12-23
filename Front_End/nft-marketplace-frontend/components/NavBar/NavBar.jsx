a8154eb1c2441d7d0f3f57574a3dd4531914c24a
js3867 <83634235+js3867@users.noreply.github.com>
2022-12-23 14:11:45 +0800

    Added global css and css for NavBar components

--- Front_End/nft-marketplace-frontend/components/NavBar/NavBar.jsx
+++ Front_End/nft-marketplace-frontend/components/NavBar/NavBar.jsx
@@ -1,184 +1,174 @@
-import React, { useState, useEffect } from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
+import React, { useState, useEffect } from "react"
+import Image from "next/image"
+import Link from "next/link"
 
 // React-icons https://react-icons.github.io/react-icons/
-import { MdNotifications } from 'react-icons/md'
-import { BsSearch } from 'react-icons/bs'
-import { CgMenuLeft, CgMenuRight } from 'react-icons/cg'
+import { MdNotifications } from "react-icons/md"
+import { BsSearch } from "react-icons/bs"
+import { CgMenuLeft, CgMenuRight } from "react-icons/cg"
 
 // INTERNAL IMPORT
-import Style from './NavBar.module.css'
-import { Discover, HelpCentre, Notification, Profile, SideBar } from './index'
-import { Button } from '../componentsindex'
-import images from '../../img/index' // note how images includes ALL exports
+import Style from "./NavBar.module.css"
+import { Discover, HelpCenter, Notification, Profile, SideBar } from "./index"
+import { Button } from "../componentsindex"
+import images from "../../img/index" // note how images includes ALL exports
 
 const NavBar = () => {
-	//-----USESTATE COMPONENTS
-	const [discover, setDiscover] = useState(false)
-	const [help, setHelp] = useState(false)
-	const [notification, setNotification] = useState(false)
-	const [profile, setProfile] = useState(false)
-	const [openSideMenu, setOpenSideMenu] = useState(false)
+  //-----USESTATE COMPONENTS
+  const [discover, setDiscover] = useState(false)
+  const [help, setHelp] = useState(false)
+  const [notification, setNotification] = useState(false)
+  const [profile, setProfile] = useState(false)
+  const [openSideMenu, setOpenSideMenu] = useState(false)
 
-	const openMenu = (e) => {
-		const btnText = e.target.innerText
-		if (btnText == 'Discover') {
-			setDiscover(true)
-			setHelp(false)
-			setNotification(false)
-			setProfile(false)
-			// setOpenSideMenu(false) // why this one not included?
-		} else if (btnText == 'Help Center') {
-			setDiscover(false)
-			setHelp(true)
-			setNotification(false)
-			setProfile(false)
-		} else {
-			setDiscover(false)
-			setHelp(false)
-			setNotification(false)
-			setProfile(false)
-		}
-	}
+  const openMenu = (e) => {
+    const btnText = e.target.innerText
+    if (btnText == "Discover") {
+      setDiscover(true)
+      setHelp(false)
+      setNotification(false)
+      setProfile(false)
+      // setOpenSideMenu(false) // why this one not included?
+    } else if (btnText == "Help Center") {
+      setDiscover(false)
+      setHelp(true)
+      setNotification(false)
+      setProfile(false)
+    } else {
+      setDiscover(false)
+      setHelp(false)
+      setNotification(false)
+      setProfile(false)
+    }
+  }
 
-	const openNotification = () => {
-		if (!notification) {
-			setDiscover(false)
-			setHelp(false)
-			setNotification(true)
-			setProfile(false)
-		} else {
-			setDiscover(false)
-			setHelp(false)
-			setNotification(false)
-			setProfile(false)
-		}
-	}
+  const openNotification = () => {
+    if (!notification) {
+      setDiscover(false)
+      setHelp(false)
+      setNotification(true)
+      setProfile(false)
+    } else {
+      setDiscover(false)
+      setHelp(false)
+      setNotification(false)
+      setProfile(false)
+    }
+  }
 
-	const openProfile = () => {
-		if(!profile){
-			setDiscover(false)
-			setHelp(false)
-			setNotification(false)
-			setProfile(true)
-		} else {
-			setProfile(false)
-		}
-	}
+  const openProfile = () => {
+    if (!profile) {
+      setDiscover(false)
+      setHelp(false)
+      setNotification(false)
+      setProfile(true)
+    } else {
+      setProfile(false)
+    }
+  }
 
-	const openSideBar = () => {
-		if(!openSideMenu){
-			setOpenSideMenu(true)
-		} else {
-			setOpenSideMenu(false)
-		}
-	}
+  const openSideBar = () => {
+    if (!openSideMenu) {
+      setOpenSideMenu(true)
+    } else {
+      setOpenSideMenu(false)
+    }
+  }
 
-	return (
-		// "if you want to convert this series into another project, taking this approach (below) will make it very easy to do"
-		<div className={Style.navbar}>
-			<div className={Style.navbar_container}>
-				<div className={Style.navbar_container_left}>
-					<div className={Style.logo}>
-						<Image
-							src={images.logo}
-							alt="NFT MARKET PLACE"
-							width={100}
-							height={100}
-						/>
-					</div>
-					<div className={Style.navbar_container_left_box_input}>
-						<div
-							className={
-								Style.navbar_container_left_box_input_box
-							}
-						>
-							<input type="text" placeholder="Search NFT" />
-							<BsSearch
-								onClick={() => { }}
-								className={Style.search_icon}
-							/>
-						</div>
-					</div>
-				</div>
+  return (
+    // "if you want to convert this series into another project, taking this approach (below) will make it very easy to do"
+    <div className={Style.navbar}>
+      <div className={Style.navbar_container}>
+        <div className={Style.navbar_container_left}>
+          <div className={Style.logo}>
+            <Image
+              src={images.logo}
+              alt="NFT MARKET PLACE"
+              width={100}
+              height={100}
+            />
+          </div>
+          <div className={Style.navbar_container_left_box_input}>
+            <div className={Style.navbar_container_left_box_input_box}>
+              <input type="text" placeholder="Search NFT" />
+              <BsSearch onClick={() => {}} className={Style.search_icon} />
+            </div>
+          </div>
+        </div>
 
-				{/* END OF LEFT SECTION */}
-				<div className={Style.navbar_container_right}>			
-					<div className={Style.navbar_container_right_discover}>
-						{/* DISCOVER MENU */}
-						<p onClick={(e) => openMenu(e)}>Discover</p>
-						{discover && (
-							<div
-								className={
-									Style.navbar_container_right_discover_box}>
-								<Discover />
-							</div>
-						)}
-					</div>
+        {/* END OF LEFT SECTION */}
+        {/* START OF RIGHT SECTION */}
+        <div className={Style.navbar_container_right}>
+          <div className={Style.navbar_container_right_discover}>
+            {/* DISCOVER MENU */}
+            <p onClick={(e) => openMenu(e)}>Discover</p>
+            {discover && (
+              <div className={Style.navbar_container_right_discover_box}>
+                <Discover />
+              </div>
+            )}
+          </div>
 
-					{/* HELP CENTRE MENU */}
-					<div className={Style.navbar_container_right_help}>
-						<p onClick={(e) => openMenu(e)}>Help Center</p>
-						{help && (
-							<div className={Style.navbar_container_right_help_box}>
-								<HelpCentre />
-							</div>
-						)}
-					</div>
+          {/* HELP CENTER MENU */}
+          <div className={Style.navbar_container_right_help}>
+            <p onClick={(e) => openMenu(e)}>Help Center</p>
+            {help && (
+              <div className={Style.navbar_container_right_help_box}>
+                <HelpCenter />
+              </div>
+            )}
+          </div>
 
-					{/* NOTIFICATION */}
-					<div className={Style.navbar_container_right_notify}>
-						<MdNotifications 
-							className={Style.notify} 
-							onClick={() => openNotification()}
-						/>
-						{notification && <Notification />}
-					</div>
-				
-				{/* CREATE BUTTON SECTION */}
-				<div className={Style.navbar_container_right_button}>
-					{/* because every button will be different, we need to add unqiue props for each blueprint component*/}
-					<Button btnText='Create'/>
-				</div>
+          {/* NOTIFICATION */}
+          <div className={Style.navbar_container_right_notify}>
+            <MdNotifications
+              className={Style.notify}
+              onClick={() => openNotification()}
+            />
+            {notification && <Notification />}
+          </div>
 
-				{/* USER PROFILE */}
+          {/* CREATE BUTTON SECTION */}
+          <div className={Style.navbar_container_right_button}>
+            {/* because every button will be different, we need to add unqiue props for each blueprint component*/}
+            <Button btnText="Create" />
+          </div>
 
-				<div className={Style.navbar_container_right_profile_box}>
-					<div className={Style.navbar_container_right_profile}>
-						<Image 
-							scr={images.user1} 
-							alt="Profile" 
-							width={40} 
-							height={40} 
-							onClick={()=>openProfile()}
-							className={Style.navbar_container_right_profile}
-						/>
+          {/* USER PROFILE */}
 
-						{/* "if profile, we have to render the profile component" */}
-						{profile && <Profile />}
-					</div>
-				</div>
+          <div className={Style.navbar_container_right_profile_box}>
+            <div className={Style.navbar_container_right_profile}>
+              <Image
+                scr={images.user1}
+                alt="Profile"
+                width={40}
+                height={40}
+                onClick={() => openProfile()}
+                className={Style.navbar_container_right_profile}
+              />
 
-				{/* MENU BUTTON -- mobile */}
-				<div className={Style.navbar_container_right_menuBtn}>
-						<CgMenuRight className={Style.menuIcon}
-							onClick={()=>openSideBar()}
-						/>
-				</div>
+              {/* "if profile, we have to render the profile component" */}
+              {profile && <Profile />}
+            </div>
+          </div>
 
-				</div> 
-			</div>
-		
-		{/* SIDEBAR COMPONENT -- only mobile (hence "out of inner-component") */}
-		{
-			openSideMenu && (
-				<div className={Style.SideBar}>
-					<SideBar serOpenSideMenu={setOpenSideMenu} />
-				</div>
-			)
-		}
-		</div>
-		)
+          {/* MENU BUTTON -- mobile */}
+          <div className={Style.navbar_container_right_menuBtn}>
+            <CgMenuRight
+              className={Style.menuIcon}
+              onClick={() => openSideBar()}
+            />
+          </div>
+        </div>
+      </div>
+
+      {/* SIDEBAR COMPONENT -- only mobile (hence "out of inner-component") */}
+      {openSideMenu && (
+        <div className={Style.sideBar}>
+          <SideBar serOpenSideMenu={setOpenSideMenu} />
+        </div>
+      )}
+    </div>
+  )
 }
 export default NavBar
