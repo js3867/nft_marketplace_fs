c207d7efd56d377b0256adebe7253818d026ba75
js3867 <83634235+js3867@users.noreply.github.com>
2023-02-09 23:26:02 +0800

    updated button to pass multiple functions

--- Frontend/components/Button/Button.jsx
+++ Frontend/components/Button/Button.jsx
@@ -4,10 +4,15 @@ import React from "react"
 import Style from "./Button.module.css"
 
 // receive 2 props: name of button and the function to act on button
-const Button = ({ btnName, handleClick }) => {
+const Button = ({ btnName, handleClick, icon, classStyle }) => {
   return (
     <div className={Style.box}>
-      <button className={Style.button} onClick={handleClick()}>
+      <button
+        className={`${Style.button} ${classStyle}`}
+        // {`${this} ${is} ${how}`} you pass multiple functions to a button
+        onClick={handleClick()}
+      >
+        {icon}
         {btnName}
       </button>
     </div>
