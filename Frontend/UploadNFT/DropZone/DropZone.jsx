610657d225f5df5ee17313103f2548f60f6372e1
js3867 <83634235+js3867@users.noreply.github.com>
2023-02-10 14:27:00 +0800

    Added file upload+dropzone

---
+++ Frontend/UploadNFT/DropZone/DropZone.jsx
@@ -0,0 +1,115 @@
+import React, { useState, useCallback } from "react"
+import { useDropzone } from "react-dropzone"
+import Image from "next/image"
+
+//INTRNAL IMPORT
+import Style from "./DropZone.module.css"
+import images from "../../img"
+
+const DropZone = ({
+  title,
+  heading,
+  subHeading,
+  name,
+  website,
+  description,
+  royalties,
+  fileSize,
+  category,
+  properties,
+  uploadToIPFS,
+  setImage,
+}) => {
+  const [fileUrl, setFileUrl] = useState(null)
+
+  const onDrop = useCallback(async (acceptedFile) => {
+    const url = await acceptedFile[0]
+    setFileUrl(url)
+    setImage(url)
+    console.log(url)
+  })
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: "image/*",
+    maxSize: 5242880, // 5MB
+  })
+  return (
+    <div className={Style.DropZone}>
+      <div className={Style.DropZone_box} {...getRootProps()}>
+        <input {...getInputProps()} />
+        <div className={Style.DropZone_box_input}>
+          <p>{title}</p>
+          <div className={Style.DropZone_box_input_img}>
+            <Image
+              src={images.upload}
+              alt="upload"
+              width={100}
+              height={100}
+              className={Style.DropZone_box_input_img_img}
+            />
+          </div>
+          <p>{heading}</p>
+          <p>{subHeading}</p>
+        </div>
+      </div>
+
+      {/* once we have uploaded/dropped an iamge, display the preview */}
+      {fileUrl && (
+        <aside className={Style.DropZone_box_aside}>
+          <div className={Style.DropZone_box_aside_box}>
+            <Image
+              src={images.nft_image_1}
+              alt="nft image"
+              width={200}
+              height={200}
+            />
+            {/* ^^^above, using dynamic input: */}
+            {/* <Image src={fileUrl} alt="nft image" width={200} height={200} /> */}
+
+            <div className={Style.DropZone_box_aside_box_preview}>
+              <div className={Style.DropZone_box_aside_box_preview_one}>
+                <p>
+                  <samp>NFT Name:</samp>
+                  {name || ""}
+                </p>
+                <p>
+                  <samp>Website:</samp>
+                  {website || ""}
+                </p>
+              </div>
+
+              <div className={Style.DropZone_box_aside_box_preview_two}>
+                <p>
+                  <span>Description</span>
+                  {description || ""}
+                </p>
+              </div>
+
+              <div className={Style.DropZone_box_aside_box_preview_three}>
+                <p>
+                  <span>Royalties</span>
+                  {royalties || ""}
+                </p>
+                <p>
+                  <span>FileSize</span>
+                  {fileSize || ""}
+                </p>
+                <p>
+                  <span>Properties</span>
+                  {properties || ""}
+                </p>
+                <p>
+                  <span>Category</span>
+                  {category || ""}
+                </p>
+              </div>
+            </div>
+          </div>
+        </aside>
+      )}
+    </div>
+  )
+}
+
+export default DropZone
---
+++ Frontend/UploadNFT/UploadNFT.jsx
@@ -0,0 +1,246 @@
+import React, { useState } from "react"
+import { MdOutlineHttp, MdOutlineAttachFile } from "react-icons/md"
+import { FaPercent } from "react-icons/fa"
+import { AiTwotonePropertySafety } from "react-icons/ai"
+import { TiTick } from "react-icons/ti"
+import Image from "next/image"
+
+//INTERNAL IMPORT
+import Style from "./Upload.module.css"
+import formStyle from "../AccountPage/Form/Form.module.css" // take the form style from account page
+import images from "../img"
+import { Button } from "../components/componentsindex.js"
+import { DropZone } from "../UploadNFT/uploadNFTIndex.js"
+
+const UploadNFT = () => {
+  // use many states to store the various data
+  // which will be used dynamically to create the NFT
+  const [price, setPrice] = useState("")
+  const [active, setActive] = useState(0)
+  const [name, setName] = useState("")
+  const [website, setWebsite] = useState("")
+  const [description, setDescription] = useState("")
+  const [royalties, setRoyalties] = useState("")
+  const [fileSize, setFileSize] = useState("")
+  const [category, setCategory] = useState(0)
+  const [properties, setProperties] = useState("")
+  const [image, setImage] = useState(null)
+
+  const categoryArray = [
+    {
+      image: images.nft_image_1,
+      category: "Sports",
+    },
+    {
+      image: images.nft_image_2,
+      category: "Arts",
+    },
+    {
+      image: images.nft_image_3,
+      category: "Music",
+    },
+    {
+      image: images.nft_image_1,
+      category: "Digital",
+    },
+    {
+      image: images.nft_image_2,
+      category: "Time",
+    },
+    {
+      image: images.nft_image_3,
+      category: "Photos",
+    },
+  ]
+
+  return (
+    <div className={Style.upload}>
+      <DropZone
+        // use vague names for the props for multiple use
+        title="JPG, PNG, WEBM , MAX 100MB"
+        heading="Drag & drop file"
+        subHeading="or browse media on your device"
+        name={name}
+        website={website}
+        description={description}
+        royalties={royalties}
+        fileSize={fileSize}
+        category={category}
+        properties={properties}
+        // setImage={images.upload}
+        setImage={setImage}
+        // uploadToIPFS={uploadToIPFS}
+      />
+
+      <div className={Style.upload_box}>
+        <div className={formStyle.Form_box_input}>
+          <label htmlFor="nft">Item Name</label>
+          <input
+            type="text"
+            placeholder="shoaib bhai"
+            className={formStyle.Form_box_input_userName}
+            onChange={(e) => setName(e.target.value)}
+            //^^ 'e' is event, can be a click, data-entry, etc.
+          />
+        </div>
+
+        <div className={formStyle.Form_box_input}>
+          <label htmlFor="website">Website</label>
+          <div className={formStyle.Form_box_input_box}>
+            <div className={formStyle.Form_box_input_box_icon}>
+              <MdOutlineHttp />
+            </div>
+
+            <input
+              type="text"
+              placeholder="website"
+              onChange={(e) => setWebsite(e.target.value)}
+            />
+          </div>
+
+          <p className={Style.upload_box_input_para}>
+            Ciscrypt will include a link to this URL on this item's detail page,
+            so that users can click to learn more about it. You are welcome to
+            link to your own webpage with more details.
+          </p>
+        </div>
+
+        <div className={formStyle.Form_box_input}>
+          <label htmlFor="description">Description</label>
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="6"
+            placeholder="Description of your upload..."
+            onChange={(e) => setDescription(e.target.value)}
+          ></textarea>
+          <p>
+            The description will be included on the item's detail page
+            underneath its image. Markdown syntax is supported.
+          </p>
+        </div>
+
+        <div className={formStyle.Form_box_input}>
+          <label htmlFor="name">Choose collection</label>
+          <p className={Style.upload_box_input_para}>
+            Choose an exiting collection or create a new one
+          </p>
+
+          <div className={Style.upload_box_slider_div}>
+            {categoryArray.map((el, i) => (
+              <div
+                className={`${Style.upload_box_slider} ${
+                  active == i + 1 ? Style.active : ""
+                }`}
+                key={i + 1}
+                onClick={() => (setActive(i + 1), setCategory(el.category))}
+              >
+                <div className={Style.upload_box_slider_box}>
+                  <div className={Style.upload_box_slider_box_img}>
+                    <Image
+                      src={el.image}
+                      alt="background image"
+                      width={70}
+                      height={70}
+                      className={Style.upload_box_slider_box_img_img}
+                    />
+                  </div>
+                  <div className={Style.upload_box_slider_box_img_icon}>
+                    <TiTick />
+                  </div>
+                </div>
+                <p>Crypto Legend -–– {el.category} </p>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className={formStyle.Form_box_input_social}>
+          <div className={formStyle.Form_box_input}>
+            <label htmlFor="Royalties">Royalties</label>
+            <div className={formStyle.Form_box_input_box}>
+              <div className={formStyle.Form_box_input_box_icon}>
+                <FaPercent />
+              </div>
+              <input
+                type="text"
+                placeholder="20%"
+                onChange={(e) => setRoyalties(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className={formStyle.Form_box_input}>
+            <label htmlFor="size">Size</label>
+            <div className={formStyle.Form_box_input_box}>
+              <div className={formStyle.Form_box_input_box_icon}>
+                <MdOutlineAttachFile />
+              </div>
+              <input
+                type="text"
+                placeholder="165MB"
+                onChange={(e) => setFileSize(e.target.value)}
+              />
+            </div>
+          </div>
+          {/* <div className={formStyle.Form_box_input}>
+            <label htmlFor="Properties">Properties</label>
+            <div className={formStyle.Form_box_input_box}>
+              <div className={formStyle.Form_box_input_box_icon}>
+                <AiTwotonePropertySafety />
+              </div>
+              <input
+                type="text"
+                placeholder="Properties"
+                onChange={(e) => setProperties(e.target.value)}
+              />
+            </div>
+          </div> */}
+
+          <div className={formStyle.Form_box_input}>
+            <label htmlFor="Price">Price</label>
+            <div className={formStyle.Form_box_input_box}>
+              <div className={formStyle.Form_box_input_box_icon}>
+                <AiTwotonePropertySafety />
+              </div>
+              <input
+                type="text"
+                placeholder="Price"
+                onChange={(e) => setPrice(e.target.value)}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className={Style.upload_box_btn}>
+          <Button
+            btnName="Upload"
+            handleClick={
+              async () => {}
+              // createNFT(
+              //   name,
+              //   price,
+              //   image,
+              //   description,
+              //   router
+              //   // website,
+              //   // royalties,
+              //   // fileSize,
+              //   // category,
+              //   // properties
+              // )
+            }
+            classStyle={Style.upload_box_btn_style}
+          />
+          <Button
+            btnName="Preview"
+            handleClick={() => {}}
+            classStyle={Style.upload_box_btn_style}
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default UploadNFT
